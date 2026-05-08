50a278721660d5b3de47ee8e8e91dbf971c2e007
rmerces <ricardo.merces@gmail.com>
2026-05-08 11:01:28 -0300

    refactor main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu_26" {
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-26.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-*-26.04-amd64-server-*"]
   }
 }
 
